cce165b3a85393f3dadd4145e37ce79e8dd1e516
Abdelali <rmptxf@gmail.com>
2020-12-20 15:46:40 +0100

    Update home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,12 @@
 import { Component, NgZone } from '@angular/core';
 import { NavController, AlertController,ToastController, LoadingController } from '@ionic/angular';
-import { NavigationExtras } from '@angular/router';
 import { BLE } from '@ionic-native/ble/ngx';
 import { Platform } from '@ionic/angular'; 
 
 const CUSTOM_SERVICE = "ffe0" ;  
 
+//var scannedDevice = { name: '', id:'', rssi:'',  av : 0 };
+  
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -13,10 +14,9 @@ const CUSTOM_SERVICE = "ffe0" ;
 })
 export class HomePage {
 
+
   devices: any[] = [];
-  devicePlatform: string = '';
-  deviceMac:string='';
-  flagByte:string='';
+
   constructor(private ble: BLE,
               public platform: Platform,
               public navCtrl: NavController,           
@@ -26,171 +26,96 @@ export class HomePage {
               private ngZone: NgZone) {
              }
 
-
-  // Checking which platform (ios/android) the device uses
-  platformCheck()
-  {   
-    if (this.platform.is('ios')) 
-    {
-      this.devicePlatform = 'ios'; 
-      console.log('I am an iOS device!');         
-    }
-    else if(this.platform.is('android'))
-    {
-      this.devicePlatform = 'android'; 
-      console.log('I am an Android device!');       
-    }
-   // console.log(this.platform.platforms());
-  }
-
-  ionViewWillEnter()
-  {
-      console.log('ionViewWillEnter'); 
-      this.platformCheck();
-      this.blecheck()            
-  };
-              
-              
-  ionViewDidLoad() 
-  {
-      console.log('ionViewDidLoad');
-      this.blecheck()
-  };
-      
-           
+         
   scan() 
   {
-      this.onscaning();       
-      this.devices = [];  // clear existing list
-      
-      this.ble.scan([CUSTOM_SERVICE],3).subscribe(
-                device => { this.onDiscoveredDevice(device);},
-                error =>  { this.showAlert("Error","Error scanning for hm10 based devices. Make sure the Location service is enabled!");}              
-        );
-  
-      }                    
-       
-  blecheck(){                
-    this.ble.enable().then(
-              () =>  this.scan(),
-              () =>  this.bledisabled()
-                );                  
-              }
-            
-         
-  bledisabled(){  
+    this.onscaning();        
+    this.devices = [];  // clear existing list
+    
+    this.ble.scan([CUSTOM_SERVICE], 3).subscribe(
+              device => { this.onDiscoveredDevice(device);},
+              error =>  { this.showAlert("Error Scanning","Make sure you have both the Bluetooth and the Location service enabled!");}              
+      );
+  }                    
 
-    this.showAlert("Bluetooth Disabled" , "Please Enable the Bluetooth!");
-            
- }   
-
-  onDiscoveredDevice(device) 
+  onDiscoveredDevice(device : any)
   {
-    var advData;
-    var deviceMac = '';
-    var flagByte ='';
-    var s ='';
-    var p =':';  
+    var advData ;      
+    var advDataString = new Array; 
+    var deviceMac = '';       
+    var deviceFlagByte ='';    
+    var deviceBattByte ='';    
+    var s =':';                
 
-    if(this.devicePlatform == 'android')
-    {
-      advData = new Uint8Array(device.advertising);
-    }
-    else if(this.devicePlatform == 'ios')
-    {
-      advData = new Uint8Array(device.advertisting.kCBAdvDataManufacturerData);
-    }
+    advData = new Uint8Array(device.advertisting.kCBAdvDataManufacturerData);
 
     for(var i=0; i<advData.length; i++)
+    {
+      advDataString[i] = advData[i].toString(16).toUpperCase().length < 2 ? '0'+advData[i].toString(16).toUpperCase() : advData[i].toString(16).toUpperCase();
+    }
+
+    // getting the scanned device MAC address
+    // more info : HM-10_11 datasheet.pdf page 17
+    for(var i=0; i<advDataString.length; i++)
     { 
       //48, 4D, XX, XX, XX, XX, XX, XX
-      if( advData[i].toString(16).toUpperCase()   == '48' 
-        && advData[i+1].toString(16).toUpperCase() == '4D')
+      if( advDataString[i+0] == '48' 
+      && advDataString[i+1] == '4D')
       {
+        // i+2 to i+2+6(MAC size) : will hold the MAC
         for(var j=i+2; j< i+2+6; j++)
         {
-          if(advData[j].toString(16).length<2) s = '0';
-          else s = ''
-          if(j == i+2+6-1) p ='';
-          deviceMac += s + advData[j].toString(16).toUpperCase()+ p;
+          if(j == i+2+6-1) s='';
+          deviceMac += advDataString[j]+s;
         }
-      }
-      // 16, 00, B0, [FLAG]
-      else if( advData[i].toString(16).toUpperCase()   == '16' 
-            && advData[i+1].toString(16).toUpperCase() == '00'
-            && advData[i+2].toString(16).toUpperCase() == 'B0')
+      } 
+      // 07, 16, 00, B0, [FLAG], [temperature], [humidity], [battery]
+      // more info : HM-10_11 datasheet.pdf page 25
+      else if(   advDataString[i+0] == '07'
+              && advDataString[i+1] == '16' 
+              && advDataString[i+2] == '00'
+              && advDataString[i+3] == 'B0')
       {
-        flagByte = advData[i+3].toString(16).toUpperCase();
+        deviceFlagByte = advDataString [i+4];
+        deviceBattByte = (advDataString [i+5] == '42' && advDataString [i+7] == '4C') ? '100' : (advDataString[i+5][1] + advDataString[i+7][1]);
       }
-
     }
-  
-    console.log('device  id : '+ device.id);
-    console.log('device MAC : '+ deviceMac); 
-    console.log('device  id should be the same as the device MAC address on android.'); 
-    console.log('Flag : '+ flagByte);
-  
 
-    this.ngZone.run(
-      () => {
-            this.devices.push(device);
-            this.deviceMac = deviceMac;
-            this.flagByte = flagByte;
-          });
+    var scannedDevice = { name: device.name, id: device.id, mac: deviceMac, flag: deviceFlagByte, batt: deviceBattByte }; 
+
+    console.log('Scanned device  : '+ JSON.stringify(scannedDevice));
+    console.log('Scanned device advData: '+ advDataString+'.');
+
+    this.ngZone.run(() => 
+    {
+      this.devices.push(scannedDevice);
+    })
+    
+  }    
+    
+  async showAlert(title, message) 
+  {               
+    let alert = await this.alertCtrl.create({
+          header: title,
+          subHeader: message,
+          buttons: ['OK']
+          })         
+    await alert.present()
   }
-        
-    
-  deviceSelected(device) { 
-        
-      console.log(JSON.stringify(device) + ' selected'); 
-      let navigationExtras: NavigationExtras = {
-            queryParams: {
-                device: JSON.stringify(device)
-                    }
-                };
-                
-      this.navCtrl.navigateForward(['detail'],navigationExtras);
-      this.onDeviceConnected(); 
-      
-           }
-    
-            
-  async showAlert(title, message) {               
-        let alert = await this.alertCtrl.create({
-              header: title,
-              subHeader: message,
-              buttons: ['OK']
-              })         
-        await alert.present()
-       }
                 
                               
-  async onscaning() {
-         const loading = await this.loadingController.create({
-                spinner:"bubbles",
-                duration: 1000,
-                translucent: true,
-                mode: "ios",
-                cssClass: 'spinner'
-                  });
-                  return await loading.present();
-              }
+  async onscaning() 
+  {
+      const loading = await this.loadingController.create({
+          spinner:"bubbles",
+          duration: 3000,
+          translucent: true,
+          mode: "ios",
+          cssClass: 'spinner'
+            });
+        return await loading.present();
+}
               
- 
-  async onDeviceConnected() {
-          let toast = await this.toastCtrl.create({             
-                color: "primary",
-                message: " Connected",
-                duration: 1000,
-                position: 'bottom'              
-                });
-              
-          toast.onDidDismiss().then(() => {
-                  console.log('Dismissed toast');
-                });
-              
-          toast.present();
-         }
-              
+  
 
 }
